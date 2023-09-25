96f048a554c152b0d25e5ad44cd917755c95907d
chaitanya <69865263+ChaitanyaSirivuri@users.noreply.github.com>
2023-09-26 00:13:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pycaret import classification
 from pycaret import regression
 from pycaret import clustering
 import pandas as pd
+import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 import os
 
